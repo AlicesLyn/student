93348fc657762b8a331357acfbfd68fdb45e0609
Alice_Qxp <2031567814@qq.com>
2018-12-15 17:44:16 +0800

    第一次尝试

---
+++ src/main/java/com/tt/student/pojo/User.java
@@ -0,0 +1,16 @@
+package com.tt.student.pojo;
+
+/**
+ * @ProjectName: student
+ * @Package: com.tt.student.pojo
+ * @ClassName: User
+ * @Description: java类作用描述
+ * @Author: Alice_Qxp
+ * @CreateDate: 2018/11/15 20:08
+ * @UpdateUser: Alice_Qxp
+ * @UpdateDate: 2018/11/15 20:08
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+public class User {
+}
